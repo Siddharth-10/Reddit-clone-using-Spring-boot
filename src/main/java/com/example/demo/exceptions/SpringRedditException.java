24b52af0c65f2997342a13a9dd5d14c17fdafb60
Siddharth-10 <siddharthawana.work@gmail.com>
2020-09-14 21:20:06 +0530

    init

---
+++ src/main/java/com/example/demo/exceptions/SpringRedditException.java
@@ -0,0 +1,9 @@
+package com.example.demo.exceptions;
+
+public class SpringRedditException extends RuntimeException{
+
+    public SpringRedditException(String exMessage){
+        super(exMessage);
+
+    }
+}
